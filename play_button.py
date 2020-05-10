06810970560a7e5f848bb7baf8959c6205143be1
zhiyiYo <1319158137@qq,com>
2020-05-10 20:02:46 +0800

    播放按钮

--- play_button.py
+++ play_button.py
@@ -2,27 +2,27 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QBitmap, QPainter, QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication,QPushButton
 
 
-class Mask(QWidget):
+class SongerPlayButton(QPushButton):
 
     def __init__(self):
         super().__init__()
 
-        self.mask = QBitmap('resource\\images\\mask.png')
+        self.mask = QBitmap('resource\\images\\mask.svg')
         self.resize(self.mask.size())
         self.setMask(self.mask)
 
     def paintEvent(self, e):
         painter = QPainter(self)
-        pix = QPixmap('resource\\Songer Photos\\Carpenters\\Carpenters.jpg').scaled(
+        pix = QPixmap('resource\\images\\播放按钮2.png').scaled(
             200, 200, Qt.KeepAspectRatio)
         painter.drawPixmap(0, 0, self.width(), self.height(), pix)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    demo = Mask()
+    demo = SongerPlayButton()
     demo.show()
     sys.exit(app.exec_())
