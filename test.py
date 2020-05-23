981758f3917bf50297bb34cc6d065f7839c0b6df
zhiyiYo <1319158137@qq.com>
2020-05-23 14:55:19 +0800

    test

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-import json
-with open('Data\\initProfile.json') as f:
-    profile = json.load(f)
-profile['isFirstTimeToRun'] = False
-with open('Data\\initProfile.json','w') as f:
-    json.dump(profile, f)
+
     
 
