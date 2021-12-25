2e1fddd36412aee0654aa6ad91627679183b4d50
zhiyiYo <1319158137@qq.com>
2021-12-25 23:23:07 +0800

    更新依赖

--- tests/test_kuwo_music_crawler.py
+++
@@ -1,8 +0,0 @@
-from time import time
-from common.crawler.kuwo_music_crawler import KuWoMusicCrawler
-
-crawler = KuWoMusicCrawler()
-t1 = time()
-crawler.getSingerAvatar('鎖那', 'singer_avatar')
-t2 = time()
-print(t2-t1)
